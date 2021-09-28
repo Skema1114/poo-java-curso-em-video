11d161524ce6741662cc3fa074f0b1e6a0862b4b
Rafael Aranda Martins <skema1114@hotmail.com>
2021-09-28 18:09:01 -0300

    Terminando a aula 07

--- src/Inicio.java
+++ src/Inicio.java
@@ -3,6 +3,7 @@ import aula03.Aula03;
 import aula04.Aula04;
 import aula05.Aula05;
 import aula06.Aula06;
+import aula07.Aula07;
 
 public class Inicio {
     public static void main(String[] args) {
@@ -19,6 +20,9 @@ public class Inicio {
         // aula05.pratica();
 
         Aula06 aula06 = new Aula06();
-        aula06.pratica();
+        //aula06.pratica();
+
+        Aula07 aula07 = new Aula07();
+        aula07.pratica();
     }
 }
